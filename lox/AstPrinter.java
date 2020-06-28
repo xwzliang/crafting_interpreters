a93c7143ed7c7660bdce0cf856736f8854558f8b
broliang <18706703437@163.com>
2020-06-28 22:33:57 +0800

    New lox/AstPrinter.java

---
+++ lox/AstPrinter.java
@@ -0,0 +1,30 @@
+package com.craftinginterpreters.lox;
+
+// Creates an unambiguous, if ugly, string representation of AST nodes
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.expression);
+    }
+}
